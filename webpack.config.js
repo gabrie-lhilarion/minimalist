e0a0f081f91a06d4ea76a758a6fe5bfba453588f
Gabriel Hilarion <gabrielhilarion@gmail.com>
2021-12-02 11:55:11 +0100

    Install webpack webserver

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Minimalist',
+    }),
+  ],
   mode: 'development',
   output: {
     filename: 'bundle.js',
